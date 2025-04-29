414822a9fb3ec21d9f03311e5078d5037d3eed80
Khayate2201P <140816746+Khayate2201P@users.noreply.github.com>
2025-04-30 00:57:23 +0100

    header.php

--- auth-system/includes/header.php
+++ auth-system/includes/header.php
@@ -6,7 +6,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <title>Authentication System</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
     <div class="container">
