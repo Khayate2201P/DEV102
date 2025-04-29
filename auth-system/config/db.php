e39f6cfb3418f6f92cea209f8881e846384f50f0
Khayate2201P <140816746+Khayate2201P@users.noreply.github.com>
2025-04-30 00:51:43 +0100

    db.php

--- auth-system/config/db.php
+++ auth-system/config/db.php
@@ -9,9 +9,9 @@ $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // throw exceptions on errors
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,        // fetch associative arrays
-    PDO::ATTR_EMULATE_PREPARES   => false,                   // use real prepared statements
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       
+    PDO::ATTR_EMULATE_PREPARES   => false,                   
 ];
 
 try {
