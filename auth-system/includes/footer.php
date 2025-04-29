7ceb8323c790615f4f34123daaeaec9a5f4babc8
Khayate2201P <140816746+Khayate2201P@users.noreply.github.com>
2025-04-30 00:56:34 +0100

    footer.php

--- auth-system/includes/footer.php
+++ auth-system/includes/footer.php
@@ -1,3 +1,11 @@
-</div>
-</body>
-</html>
+<footer style="
+    margin-top: 3rem;
+    padding: 1rem;
+    text-align: center;
+    background: #f8f8f8;
+    color: #555;
+    font-size: 0.9rem;
+    border-top: 1px solid #eee;
+">
+    &copy; <?= date('Y') ?> YourWebsiteName. All rights reserved.
+</footer>
